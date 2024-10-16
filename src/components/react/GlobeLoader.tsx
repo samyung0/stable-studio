230b57080dac38804b2e205f2721e61ab652ad55
Yung Chin Pang <yungchinpang999@gmail.com>
2024-10-16 11:56:57 +0800

    lazy globe

---
+++ src/components/react/GlobeLoader.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export default function () {
+	const [globeLoaded, setGlobeLoaded] = useState(false);
+	const [Globe, setGlobe] = useState<any>(null);
+	useEffect(() => {
+		if (!IntersectionObserver) return;
+		const ctaSection = document.getElementById(
+			"ctaSectionContainer",
+		) as HTMLElement | null;
+		const observer = new IntersectionObserver(async function (
+			entries,
+			observer,
+		) {
+			for (let entry of entries) {
+				if (
+					entry.target === ctaSection &&
+					entry.isIntersecting &&
+					!globeLoaded
+				) {
+					setGlobeLoaded(true);
+          observer.disconnect();
+					const GlobeContainer = (await import("./GlobeContainer")).default;
+					setGlobe(<GlobeContainer />);
+				}
+			}
+		});
+		if (!ctaSection) return;
+		observer.observe(ctaSection);
+	});
+	return <>{Globe}</>;
+}
