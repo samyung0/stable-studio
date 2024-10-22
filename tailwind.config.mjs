e370fa731ad924ca077abaeda7459dbfcb2bf247
Yung Chin Pang <yungchinpang999@gmail.com>
2024-10-22 16:47:30 +0800

    repalce globe with card

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -87,6 +87,17 @@ export default {
 					5: "hsl(var(--chart-5))",
 				},
 			},
+			keyframes: {
+				"pop-blob": {
+					"0%": { transform: "scale(1)" },
+					"33%": { transform: "scale(1.2)" },
+					"66%": { transform: "scale(0.8)" },
+					"100%": { transform: "scale(1)" },
+				},
+			},
+			animation: {
+				"pop-blob": "pop-blob 5s infinite",
+			},
 		},
 	},
 	plugins: [
